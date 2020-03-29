daeaeb41e6a4bc06d4f8ab90ca60c07916f2e14d
mezozoychic <48223873+mezozoychic@users.noreply.github.com>
2020-03-29 13:50:12 +0300

    Update B64.c

--- B64.c
+++ B64.c
@@ -18,15 +18,15 @@ void B64_encode(uint8_t *input, uint8_t *output)
     while (*input)
     {
         int24word = *input++ << 0x10;
-		int24word |= *input ? (*input++ << 0x08) : 0;
-		int24word |= *input ? *input++ : 0;
+	int24word |= *input ? (*input++ << 0x08) : 0;
+	int24word |= *input ? *input++ : 0;
 
-		*output++ = B64[(int24word >> 18) & 0x3F];
-        *output++ = B64[(int24word >> 12) & 0x3F];
-		*output++ = ((int24word >> 6) & 0x3F) ? B64[(int24word >> 6) & 0x3F] : B64_padding;
-		*output++ = (int24word & 0x3F) ? B64[int24word & 0x3F] : B64_padding;
+	*output++ = B64[(int24word >> 18) & 0x3F];
+	*output++ = B64[(int24word >> 12) & 0x3F];
+	*output++ = ((int24word >> 6) & 0x3F) ? B64[(int24word >> 6) & 0x3F] : B64_padding;
+	*output++ = (int24word & 0x3F) ? B64[int24word & 0x3F] : B64_padding;
 
-		len += 4;
+	len += 4;
     }
 
 	output[len] = '\0';
